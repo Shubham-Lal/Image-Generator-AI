ab8a546dd193e4a171d161e786267fdd46e04c26
CERTIFIED2003 <111045472+CERTIFIED2003@users.noreply.github.com>
2023-01-22 00:07:00 +0530

    Initial Commit

---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { preview } from '../assets';
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from '../components';
+
+export default function CreatePost() {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  });
+  const [generatingImg, setGeneratimgImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[25px]">Create</h1>
+        <p className="mt-2 text-justify text-[#666e75] text-[16px] max-w-[500px]">Create imaginative and visually stunning images generated through Image Generator AI and share them with the community</p>
+      </div>
+    </section>
+  )
+}
