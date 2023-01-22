125b056108f8a6cc85a9ce0c069a75fc8e7b9a47
CERTIFIED2003 <111045472+CERTIFIED2003@users.noreply.github.com>
2023-01-22 18:59:33 +0530

    Added CORS to backend server.js file

--- index.js
+++ index.js
@@ -9,8 +9,15 @@ import imgaiRoutes from "./routes/imgaiRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions));
+
 
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/img", imgaiRoutes);
