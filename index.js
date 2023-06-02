b1039d3eb9ac64839dd920c6f7aa430098cf43eb
Shubham Lal <lal.shubham027@gmail.com>
2023-06-02 22:02:35 +0530

    Added "Access-Control-Allow-Origin"

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ dotenv.config();
 const app = express();
 app.use(express.json({ limit: "50mb" }));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Set-Cookie: cross-site-cookie=whatever; SameSite=None; Secure");
+  next();
+});
+
 const corsOptions = {
     origin: '*',
     credentials: true,
